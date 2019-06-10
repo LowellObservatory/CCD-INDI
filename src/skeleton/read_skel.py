5601928a08a85533b893ebb43cbcf9255db267d0
Dyer M Lytle <dlytle@lowell.edu>
2019-06-10 09:30:52 -0700

    Update read_skel.py
    Add a comment.

--- src/skeleton/read_skel.py
+++ src/skeleton/read_skel.py
@@ -1,4 +1,6 @@
 from lxml import etree
 
+# All we do is parse the XML skeleton file to see if the DTD/XML is working.
+
 parser = etree.XMLParser(dtd_validation=True)
 tree = etree.parse("examp_skel.xml", parser)
